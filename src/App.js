1345ed74e7314140c95ae7f55d2dcbe16f692720
pedroigor66 <picoliveira92@gmail.com>
2020-06-26 18:16:38 -0300

    Listando os repositorios do backend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from "react";
+import api from './services/api';
 
 import "./styles.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Repositorio teste ${Date.now()}`,
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository])
   }
 
   async function handleRemoveRepository(id) {
     // TODO
   }
-
+//
   return (
     <div>
       <ul data-testid="repository-list">
+        {repositories.map(repository => <li key={repository.id}>{repository.title}</li>)}
         <li>
+          
           Repositório 1
 
           <button onClick={() => handleRemoveRepository(1)}>
